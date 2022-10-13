38ca26b56856359b34501f587adfa6604ffef537
Harry Nguyen <harry.nnh@gmail.com>
2022-10-13 14:56:53 +0200

    clean up functions

--- batch_load_xls.py
+++ batch_load_xls.py
@@ -2,20 +2,16 @@
 import os
 import pandas as pd
 
-path_to_file = input("Add relative path to xlsx data folder: ")
-file_names = os.listdir(path_to_file)
-file_names.sort()
-print(f"file to load {file_names}")
-
 
 def batch_load_xlsx():
+    path_to_file = input("Add relative path to xlsx data folder: ")
+    file_names = os.listdir(path_to_file)
+    file_names.sort()
+    print(f"file to load {file_names}")
     sdc_df = pd.DataFrame()
     for file_name in file_names:
         file_path = os.path.join(path_to_file, file_name)
-        ann_df = pd.read_excel(
-            file_path,
-            header=1,
-        )
+        ann_df = pd.read_excel(file_path, header=1,)
         ann_df.columns = (
             ann_df.columns.str.replace("\n", "_").str.replace(" ", "").str.lower()
         )
@@ -28,7 +24,3 @@ def batch_load_xlsx():
 if __name__ == "__main__":
     sdc_df = batch_load_xlsx()
     sdc_df.reset_index(drop=True).to_csv(input("Add relative path to csv file: "))
-
-# file_path = os.path.join(path_to_file, file_names[-1])
-# ann_df = pd.read_excel(file_path, header=1)
-# ann_df.columns = ann_df.columns.str.replace("\n", "_").str.replace(" ", "").str.lower()
--- convert_xls_csv.py
+++ convert_xls_csv.py
@@ -1,25 +1,24 @@
 import openpyxl, csv, os
 import pandas as pd
 
-path_to_xls = input("Add relative path to xlsx data folder: ")
-xls_names = os.listdir(path_to_xls)
-xls_names.sort()
-path_to_csv = input("Add relative path to csv output folder: ")
-print(f"file to convert {xls_names}")
-
 
 def xlsx_csv_converter():
-    wb = openpyxl.load_workbook(os.path.join(path_to_xls, xls_name), data_only=True)
-    sheet_name = wb.active
-    file_path = os.path.join(path_to_csv, xls_name.replace("xlsx", "csv"))
-    with open(file_path, "w", newline="") as f:
-        csv_writer = csv.writer(f)
-        for row in sheet_name.iter_rows():
-            csv_writer.writerow([cell.value for cell in row])
-    print(f"converted to {file_path}")
+    path_to_xls = input("Add relative path to xlsx data folder: ")
+    xls_names = os.listdir(path_to_xls)
+    xls_names.sort()
+    path_to_csv = input("Add relative path to csv output folder: ")
+    print(f"file to convert {xls_names}")
+    for xls_name in xls_names:
+        wb = openpyxl.load_workbook(os.path.join(path_to_xls, xls_name), data_only=True)
+        sheet_name = wb.active
+        file_path = os.path.join(path_to_csv, xls_name.replace("xlsx", "csv"))
+        with open(file_path, "w", newline="") as f:
+            csv_writer = csv.writer(f)
+            for row in sheet_name.iter_rows():
+                csv_writer.writerow([cell.value for cell in row])
+                print(f"converted to {file_path}")
     return
 
 
 if __name__ == "__main__":
-    for xls_name in xls_names:
-        xlsx_csv_converter()
+    xlsx_csv_converter()
